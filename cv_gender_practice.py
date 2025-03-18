4970e6c0ebf651bd127cacb2f573fc0c48bd9e49
abbymuir47 <amuir4@byu.edu>
2025-03-18 15:48:08 -0600

    experimenting with merging dataframes, just a little stuck

--- cv_gender_practice.py
+++ cv_gender_practice.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sys import argv
-from sklearn import RandomForestClassifier, cross_val_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score
 
 #REMEMBER: Add myself to Abby's github as a contributor so I can push the changes!
 
@@ -14,19 +16,35 @@ from sklearn import RandomForestClassifier, cross_val_score, roc_auc_score
 def main():
     try:
         if len(argv) != 5:
-            raise ValueError("Incorrect number of arguments. Please provide exactly 5 arguments.")
-       
-        input_filename = argv[1]
-        sex_col = argv[2]
-        drop_cols = argv[3]
+            raise ValueError("Incorrect number of arguments. Please provide exactly 4 arguments.")
+        #refinebio_sex 
+        expression_filename = argv[1]
+        metadata_filename = argv[2]
+        comparison_type = argv[3]
         output_filename = argv[4]
 
 
-        with open(input_filename, 'r') as readFile, open(output_filename, 'w') as writeFile:
-            df_in = pd.read_csv(readFile)
+        with open(expression_filename, 'r') as expressionFile, open(metadata_filename, 'r') as metadataFile, open(output_filename, 'w') as writeFile:
+            expression_df = pd.read_csv(expressionFile, sep='\t')
+            metadata_df = pd.read_csv(metadataFile, sep='\t')
+            # print("metadata_df: ", metadata_df.head())
 
-            #drop columns
-            df_in = df_in.drop(columns = drop_cols)
+            expression_df = expression_df.set_index('Gene')
+            expression_df.index.name = 'refinebio_accession_code'
+            print("before transposing:\n", expression_df.head())
+            # print("\n\n\n\n\n\n\n")
+
+            expression_df = expression_df.transpose()
+            #expression_df = expression_df.rename(columns={'Gene':'refinebio_accession_code'}, inplace=True)
+            #expression_df.index.name = 'refinebio_accession_code'
+            print("after transposing:\n", expression_df.head())
+            # print("#rows: ", len(expression_df))
+
+            metadata_df = metadata_df[['refinebio_accession_code', "refinebio_sex"]]
+            # print("should only be 2 columns: ", metadata_df)
+
+            merged_df = pd.merge(metadata_df, expression_df, on='refinebio_accession_code')
+            print("after merging:", merged_df.head())
 
             #only keep rows with existing sex data
             values_to_keep = ['male', 'female']
