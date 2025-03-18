4f76da8ee5153d1366c20d1c13585acab279067f
abbymuir47 <amuir4@byu.edu>
2025-03-18 14:10:04 -0600

    adding in amanda's changes

--- cv_gender_practice.py
+++ cv_gender_practice.py
@@ -1,54 +1,66 @@
 import pandas as pd
+from sys import argv
+from sklearn import RandomForestClassifier, cross_val_score, roc_auc_score
+#GSE37069 - Burn tissue
+#GSE99039 - Parkinson's
+
+
+#REMEMBER: Add myself to Abby's github as a contributor so I can push the changes!
 
 
 # use sys.argv to accept arguments - name of input file, column name w/ class labels, column names to drop (comma-separated list), description of comparison (sex, autosomal, all) name of output file to create
-# script is generic - not specific to just 1 dataset 
+# script is generic - not specific to just 1 dataset
 # output file - for each cv fold, what is the roc_auc score? - 5 rows, one for each fold
-# one column w/ name of input file - then we can merge all of the output files together at the end 
-# might need to write another script to reformat the expression data so that we can use it w the ml stuff 
+# one column w/ name of input file - then we can merge all of the output files together at the end
+# might need to write another script to reformat the expression data so that we can use it w the ml stuff
 
 
-df = pd.read_csv('combined_human_mouse_meta_v2.csv')
+def main():
+    try:
+        if len(argv) != 5:
+            raise ValueError("Incorrect number of arguments. Please provide exactly 5 arguments.")
+       
+        input_filename = argv[1]
+        sex_col = argv[2]
+        drop_cols = argv[3]
+        output_filename = argv[4]
 
-df = df.drop(columns = ['present', 'num_reads'])
-print(df.head())
 
-values_to_keep = ['male', 'female']
-df = df[df['metadata_sex'].isin(values_to_keep)]
-print(df.head())
-#metadata_sex column
-X = df.iloc[:,0:5]
-#expression_sex column
-y = df.iloc[:,6]
+        with open(input_filename, 'r') as readFile, open(output_filename, 'w') as writeFile:
+            df_in = pd.read_csv(readFile)
 
-from sklearn.model_selection import train_test_split
+            #drop columns
+            df_in = df_in.drop(columns = drop_cols)
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=17, test_size=0.2)
+            #only keep rows with existing sex data
+            values_to_keep = ['male', 'female']
+            df = df[df[sex_col].isin(values_to_keep)]
 
-from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier(random_state=17)
-rf.fit(X_train, y_train)
+            #metadata_sex column
+            X = df.iloc[:,0:5]
+            #expression_sex column
+            y = df.iloc[:,6]
 
-y_pred = rf.predict(X_test)
+            #hyper parameters
+            rf = RandomForestClassifier(n_estimators = 1000,
+                                        criterion = 'entropy',
+                                        min_samples_split = 10,
+                                        max_depth = 14,
+                                        random_state = 42
+            )
 
-from sklearn.metrics import classification_report
+            cv_scores = cross_val_score(rf, X, y, cv=5, scoring='accuracy')
+            print("Cross-validation scores for each fold:", cv_scores)
 
-features = pd.DataFrame(rf.feature_importances_, index = X.columns)
+            #Calculate ROC AUC
+            roc_auc = roc_auc_score(sex_col, y)
+            print(roc_auc)
 
-#hyper parameters
-rf2 = RandomForestClassifier(n_estimators = 1000,
-                             criterion = 'entropy',
-                             min_samples_split = 10,
-                             max_depth = 14,
-                             random_state = 42
-)
+            #create output file
 
-rf2.fit(X_train, y_train)
-#print(rf2.score(X_test, y_test))
-ypred2 = rf2.predict(X_test)
 
-#print(classification_report(y_test, ypred2))
-from sklearn.model_selection import cross_val_score
-
-cv_scores = cross_val_score(rf2, X, y, cv=5, scoring='accuracy')
-print("Cross-validation scores for each fold:", cv_scores)
+    except ValueError as ve:
+        print(f"Error: {ve}")
+       
+if __name__ == "__main__":
+    main()
